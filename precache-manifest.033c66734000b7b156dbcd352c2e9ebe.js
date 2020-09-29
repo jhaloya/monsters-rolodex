f9bbeee7988d8ad52668c98b5fa8e38997a4b207
User-WFH <User-WFH@01hw826921.India.TCS.com>
2020-09-29 22:52:59 +0530

    Updates

---
+++ precache-manifest.033c66734000b7b156dbcd352c2e9ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33834daa325551037d617a6bec1df845",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa9c316bbc49f9d32a7b",
+    "url": "/monsters-rolodex/static/css/main.4c9d151b.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9c316bbc49f9d32a7b",
+    "url": "/monsters-rolodex/static/js/main.863070d0.chunk.js"
+  },
+  {
+    "revision": "99aeed3ea4362ed85205",
+    "url": "/monsters-rolodex/static/js/runtime-main.dcaacdb6.js"
+  }
+]);
